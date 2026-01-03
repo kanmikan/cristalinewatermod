24bc68159b830e16b7d0cdf10a6863e20370ae6a
kanmikan <soloquieronp@gmail.com>
2026-01-03 00:31:08 -0300

    solo tintar cuando hay agua y solo ese quad. (#5)

--- src/main/java/com/mikanon/cristalinewater/mixin/MixinRenderBlocks.java
+++ src/main/java/com/mikanon/cristalinewater/mixin/MixinRenderBlocks.java
@@ -6,11 +6,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockCauldron;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderBlocks.class)
 public abstract class MixinRenderBlocks {
@@ -18,11 +21,28 @@ public abstract class MixinRenderBlocks {
     @Shadow
     public IBlockAccess blockAccess;
 
+    /*
     @Redirect(method = "renderBlockCauldron", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockCauldron;colorMultiplier(Lnet/minecraft/world/IBlockAccess;III)I"))
     private int onCauldronColorMultiplier(BlockCauldron block, IBlockAccess world, int x, int y, int z) {
+        if (world.getBlockMetadata(x, y, z) == 0) return block.colorMultiplier(world, x, y, z);
         //TODO this is assuming the cauldron can only have normal water, not modded liquids.
         int[] average = BiomeColors.averageColorBlend(world, x, z, Config.DEFAULT_BIOME_BLEND_RADIUS);
         return ((average[0] / average[3]) << 16) | ((average[1] / average[3]) << 8) | (average[2] / average[3]);
+    }
+    */
+
+    @Inject(method = "renderBlockCauldron", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderBlocks;renderFaceYPos(Lnet/minecraft/block/Block;DDDLnet/minecraft/util/IIcon;)V", ordinal = 1, shift = At.Shift.BEFORE))
+    private void onBeforeCauldronWater(BlockCauldron block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        int meta = this.blockAccess.getBlockMetadata(x, y, z);
+        if (meta == 0) return;
+
+        int[] avg = BiomeColors.averageColorBlend(this.blockAccess, x, z, Config.DEFAULT_BIOME_BLEND_RADIUS);
+        Tessellator.instance.setColorOpaque((avg[0] / avg[3]), (avg[1] / avg[3]), (avg[2] / avg[3]));
+    }
+
+    @Inject(method = "renderBlockCauldron", at = @At("RETURN"))
+    private void onAfterCauldronWater(BlockCauldron block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        Tessellator.instance.setColorOpaque_F(1.0F, 1.0F, 1.0F);
     }
 
     @Redirect(method = "renderBlockLiquid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;colorMultiplier(Lnet/minecraft/world/IBlockAccess;III)I"))
