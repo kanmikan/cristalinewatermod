19cfe081f3a6896aba901db2c91db01643796539
kanmikan <soloquieronp@gmail.com>
2026-01-03 01:46:08 -0300

    version bump

--- src/main/java/com/mikanon/cristalinewater/CristalineWater.java
+++ src/main/java/com/mikanon/cristalinewater/CristalineWater.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class CristalineWater {
 	
 	public static final String MODID = "cristalinewater";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event){
